1c1fbfe5c29fea8876bdac15614e4c8a5c2ac2fa
boymav <boymav@gmail.com>
2026-02-17 13:03:24 -0300

    Update doramas.js

--- doramas.js
+++ doramas.js
@@ -21,7 +21,8 @@ Entre momentos de humor, tensão e romance proibido,
 o casal enfrenta escolhas difíceis e grandes sacrifícios.
 
 É uma história emocionante sobre amor que ultrapassa fronteiras e ideologias.`,
- imagem:"https://raw.githubusercontent.com/boymav/clube-do-dorama/main/imagens/doramas/crash-landing.jpg"
+ imagem:"https://raw.githubusercontent.com/boymav/clube-do-dorama/main/imagens/doramas/crash-landing.jpg",
+ trailer:"https://www.youtube.com/embed/eXMjTXL2Vks"
 }
 ,
 {
