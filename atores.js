8333338923b43b3da64c7dc3b270c749559d555d
boymav <boymav@gmail.com>
2026-04-19 10:03:01 -0300

    Update atores.js

--- atores.js
+++ atores.js
@@ -10,8 +10,27 @@ const atores = [
     link: "atores/kim-soo-hyun.html",
     foto: "https://raw.githubusercontent.com/boymav/clube-do-dorama/refs/heads/main/atores/kim%20soo%20hyun.jpg",
     resumo: "O ator mais bem pago da Coreia e astro de Queen of Tears."
+  },
+const atores = [
+  {
+    nome: "IU (Lee Ji-eun)",
+    link: "atores/iu.html",
+    foto: "https://raw.githubusercontent.com/boymav/clube-do-dorama/refs/heads/main/atores/i.u.webp",
+    resumo: "A rainha da Coreia e estrela de Hotel Del Luna."
+  },
+  {
+    nome: "Kim Soo-hyun",
+    link: "atores/kim-soo-hyun.html",
+    foto: "https://raw.githubusercontent.com/boymav/clube-do-dorama/refs/heads/main/atores/kim%20soo%20hyun.jpg",
+    resumo: "O astro de Rainha das Lágrimas e ator mais bem pago."
+  },
+  {
+    nome: "Kim Ji-won",
+    link: "atores/kim-ji-won.html",
+    foto: "https://raw.githubusercontent.com/boymav/clube-do-dorama/refs/heads/main/atores/kim%20ji%20won.jpg",
+    resumo: "A estrela de Rainha das Lágrimas e Descendentes do Sol."
   }
-  // Quando criar a Kim Ji-won, é só adicionar a vírgula e colocar os dados dela aqui embaixo
+];
 ];
 
 // Função para renderizar os atores na página
